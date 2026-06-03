dd832817c9f9a86777323e34db0eea88bcf5aae6
Ivan Shpyakin <i.shpyakin@tinkoff.ru>
2026-06-03 19:48:12 +0300

    small fixes

--- accounts/src/main/java/ru/yandex/practicum/accounts/WebConfiguration.java
+++ accounts/src/main/java/ru/yandex/practicum/accounts/WebConfiguration.java
@@ -4,11 +4,15 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
+@EnableAsync
 @PropertySource("classpath:application.properties")
 public class WebConfiguration {
 
@@ -22,5 +26,16 @@ public class WebConfiguration {
                         .contact(new Contact()
                                 .name("Yandex Practicum")
                                 .email("support@yandex.ru")));
+    }
+
+    @Bean(name = "AccountsThreadPoolTaskExecutor")
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(24);
+        executor.setThreadNamePrefix("AccountsPool-");
+        executor.initialize();
+        return executor;
     }
 }
